342d48496397a2cbe92b57aa35415099950cddb7
Jonathan Bergen <mail@jonathanbergen.com>
2019-09-15 11:25:50 +0200

    Wip

--- app/src/main/java/com/jonathanbergen/actions/MainActivity.kt
+++ app/src/main/java/com/jonathanbergen/actions/MainActivity.kt
@@ -2,11 +2,15 @@ package com.jonathanbergen.actions
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.btnAction
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    btnAction.setOnClickListener { toast("") }
   }
+
 }
